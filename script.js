8a6e076210dcd019fac7af123beaadfee018ec78
Guadeloupe33 <stevenfrancius@gmail.com>
2023-09-25 09:43:40 -0400

    added addtiona js questions

---
+++ script.js
@@ -0,0 +1,11 @@
+const questions = [
+    {
+        question: "What does 'JS' stand for?",
+        choices: ["Java Source", "JavaScript", "Just Script", "JScript"],
+        correctAnswer: "JavaScript"
+    },
+    {
+        question: "What is the purpose of 'document.getElementById()' in JavaScript?",
+        choices: ["Retrieve an element by its ID", "Create a new element", "Remove an element", "Change the page's title"],
+        correctAnswer: "Retrieve an element by its ID"
+    },
