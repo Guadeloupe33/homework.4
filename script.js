4304fbd4f3d51ff4f1865ca4bd4557ce5ffef320
Guadeloupe33 <stevenfrancius@gmail.com>
2023-09-25 19:19:40 -0400

    added some addtional styling to the homework

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 const questions = [
     {
-        question: "What does 'JS' stand for?",
+         question: "What does 'JS' stand for?",
         choices: ["Java Source", "JavaScript", "Just Script", "JScript"],
         correctAnswer: "JavaScript"
     },
     {
-        question: "What is the purpose of 'document.getElementById()' in JavaScript?",
-        choices: ["Retrieve an element by its ID", "Create a new element", "Remove an element", "Change the page's title"],
-        correctAnswer: "Retrieve an element by its ID"
+        question: "What is the meaning of life?",
+         choices: ["I don't know", "still trying to figure it out", " that is a great qestion", "to become a software developer"],
+        correctAnswer: "still trying to figure it out"
     },
 ];
 
 let currentQuestionIndex = 0;
-let score = 0;
+               let score = 0;
 let timer;
-const questionContainer = document.getElementById("question-container");
+ const questionContainer = document.getElementById("question-container");
 const choicesContainer = document.getElementById("choices-container");
-const timerContainer = document.getElementById("timer");
+  const timerContainer = document.getElementById("timer");
 const scoreContainer = document.getElementById("score");
 const startButton = document.getElementById("start-button");
-const gameOverContainer = document.getElementById("game-over-container");
+  const gameOverContainer = document.getElementById("game-over-container");
 const finalScoreContainer = document.getElementById("final-score");
 const initialsInput = document.getElementById("initials");
 const saveScoreButton = document.getElementById("save-score");
@@ -37,7 +37,7 @@ function nextQuestion() {
         choicesContainer.innerHTML = "";
         currentQuestion.choices.forEach((choice) => {
             const choiceButton = document.createElement("button");
-            choiceButton.textContent = choice;
+        choiceButton.textContent = choice;
             choiceButton.addEventListener("click", () => checkAnswer(choice, currentQuestion.correctAnswer));
             choicesContainer.appendChild(choiceButton);
         });
@@ -70,15 +70,15 @@ function startTimer() {
         }
     }, 1000);
 }
-function endQuiz() {
-    gameOverContainer.style.display = "block";
+         function endQuiz() {
+        gameOverContainer.style.display = "block";
     finalScoreContainer.textContent = score;
 }
 
 
 saveScoreButton.addEventListener("click", () => {
-    const initials = initialsInput.value;
+                 const initials = initialsInput.value;
     
 });
 
-startButton.addEventListener("click", startQuiz);
+      startButton.addEventListener("click", startQuiz);
